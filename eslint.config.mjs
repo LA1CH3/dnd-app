824d399fbb603604bbe3a59613ea07ebebe843a6
Jay Laiche <jay@jaylaiche.com>
2026-02-05 22:16:57 -0500

    fix lint and format issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,23 +3,23 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
-import { includeIgnoreFile } from "@eslint/compat";
-import { fileURLToPath } from "node:url";
-import globals from 'globals'
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+import globals from 'globals';
 
-const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig(
-  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
-  {                                                                                               
-    languageOptions: {                                                                            
-      globals: {                                                                                  
-        ...globals.node,                                                                          
-        ...globals.browser,                                                                       
-      },                                                                                          
-    },                                                                                            
-  }
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,21 @@
-import * as fs from 'node:fs';
 import {
   queryOptions,
-  useMutation,
-  useQueryClient,
+  // useMutation,
+  // useQueryClient,
   useSuspenseQuery,
 } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { createServerFn } from '@tanstack/react-start';
+// import { createServerFn } from '@tanstack/react-start';
 import { createDatabaseFunction } from '../serverFns/database';
 import { users } from '../db/schema';
 
-const filePath = 'count.txt';
+// const filePath = 'count.txt';
 
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  );
-}
+// async function readCount() {
+//   return parseInt(
+//     await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
+//   );
+// }
 
 const getCount = createDatabaseFunction().handler(async ({ context }) => {
   const usersData = await context.db.select().from(users);
@@ -31,14 +30,14 @@ const countQuery = queryOptions({
   queryFn: () => getCount(),
 });
 
-const updateCount = createServerFn({ method: 'POST' })
-  .inputValidator((d: number) => d)
-  .handler(async ({ data }) => {
-    console.log('hello');
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
-    console.log('done');
-  });
+// const updateCount = createServerFn({ method: 'POST' })
+//   .inputValidator((d: number) => d)
+//   .handler(async ({ data }) => {
+//     console.log('hello');
+//     const count = await readCount();
+//     await fs.promises.writeFile(filePath, `${count + data}`);
+//     console.log('done');
+//   });
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -48,16 +47,16 @@ export const Route = createFileRoute('/')({
 });
 
 function Home() {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
   const { data } = useSuspenseQuery(countQuery);
 
-  const { mutate, isPending } = useMutation({
-    mutationFn: (amount: number) => updateCount({ data: amount }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['count'] });
-    },
-  });
+  // const { mutate, isPending } = useMutation({
+  //   mutationFn: (amount: number) => updateCount({ data: amount }),
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({ queryKey: ['count'] });
+  //   },
+  // });
 
   return data.map((user) => user.name);
 }
